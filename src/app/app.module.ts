403333c725ded48ebca4d1cbb8c717871e14ad30
Juan Morales <jmoralespuello@gmail.com>
2023-02-21 17:31:31 -0500

    ADD: New component Album

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
