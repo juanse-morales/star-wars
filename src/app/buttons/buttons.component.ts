537c13c8542ed07bf64ab710a500947eb687ba0a
Juan Morales <jmoralespuello@gmail.com>
2023-02-22 09:52:56 -0500

    REFACTOR: Modify selector of ButtonComponent

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-buttons',
+  selector: 'buttons',
   templateUrl: './buttons.component.html',
   styleUrls: ['./buttons.component.css']
 })
