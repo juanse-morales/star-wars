a495991fde7a67cbda3fe5178fabe9a23b4e82dc
Juan Morales <jmoralespuello@gmail.com>
2023-02-21 21:05:10 -0500

    REFACTOR: Modify selector of LogoComponent

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-logo',
+  selector: 'logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.css']
 })
