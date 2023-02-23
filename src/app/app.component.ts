630b9af25d6751c8408eda22e98485078d17f33f
Juan Morales <jmoralespuello@gmail.com>
2023-02-22 22:39:09 -0500

    REFACTOR: Configure receive title on AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'star-wars';
+
+  public title_main_container: string = '';
+
+  constructor (
+    
+  ) {
+    if(location.href.split('/')[3] === 'album'){
+      this.title_main_container = 'Mi álbum';
+    } else {
+      this.title_main_container = "Obtener láminas";
+    }
+  }
+
+  public onReceiveTitle (event:string) {
+    this.title_main_container = event;
+  }
+
 }
