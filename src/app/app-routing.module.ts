41672f6d5e2295e99d39a9be8eb07d7dd61f74cd
Juan Morales <jmoralespuello@gmail.com>
2023-02-22 21:05:41 -0500

    REFACTOR: Configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './album/album.component';
+import { EnvelopeComponent } from './envelope/envelope.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'album',
+    component: AlbumComponent
+  },
+  {
+    path: 'envelope',
+    component: EnvelopeComponent
+  },
+  {
+    path: '',
+    redirectTo: 'album',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
