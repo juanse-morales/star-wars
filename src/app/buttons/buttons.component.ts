31a58fe3b79bd9936e807e877ee31d0803378b8c
Juan Morales <jmoralespuello@gmail.com>
2023-02-22 22:38:01 -0500

    REFACTOR: Configure sending of title main

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'buttons',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent {
+  
+  @Output() public title_main: EventEmitter<string> = new EventEmitter();
+
+  public onSendTitle (title: string): void {
+    this.title_main.emit(title);
+  }
 
 }
