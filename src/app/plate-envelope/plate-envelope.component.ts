6839578792786d0303b5111376491c4a9e0e80ef
Juan Morales <jmoralespuello@gmail.com>
2023-02-27 18:27:22 -0500

    REFACTOR: Format code plate-envelope component

--- src/app/plate-envelope/plate-envelope.component.ts
+++ src/app/plate-envelope/plate-envelope.component.ts
@@ -7,25 +7,26 @@ import { trigger, state, transition, animate, style } from '@angular/animations'
   styleUrls: ['./plate-envelope.component.css'],
   animations: [
     trigger('openClose', [
-      // ...
-      state('open', style({
-        'min-width': '220px',
-        'min-height': '155px',
-        border: '2pt solid rgb(112, 161, 163)'
-      })),
-      state('closed', style({
-        'min-width': '0px',
-        'min-height': '0px',
-        border: 'none'
-      })),
-      transition('open => closed', [
-        animate('400ms 0s ease-out')
-      ]),
-      transition('closed => open', [
-        animate('500ms 0s ease-out')
-      ]),
+      state(
+        'open',
+        style({
+          'min-width': '220px',
+          'min-height': '155px',
+          border: '2pt solid rgb(112, 161, 163)',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          'min-width': '0px',
+          'min-height': '0px',
+          border: 'none',
+        })
+      ),
+      transition('open => closed', [animate('400ms 0s ease-out')]),
+      transition('closed => open', [animate('500ms 0s ease-out')]),
     ]),
-  ]
+  ],
 })
 export class PlateEnvelopeComponent {
   @Input() public isOpen: boolean = false;
